21fb8900406f10e93f97eb7e6a833bda49ce2dbf
tonyishangu <tonyishangu@gmail.com>
2026-03-19 19:28:51 +0300

    sidebar changes

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom'
 const Sidebar = () => {
     const [open, setOpen] = useState(true)
   return (
-    <div className={`${open ? 'w-64'  : 'w-20'} text-[#5D5FEF] border-r-2` }>
+    <div className={`${open ? 'w-64'  : 'w-20'} font-medium text-[#5D5FEF] border-r-2` }>
         <button className="p-4" onClick={() => setOpen(!open)}>
         {" "}
-        ☰{" "}
+        <h1>FleetIQ ☰{" "}</h1>
       </button>
-      <nav className="mt-5">
+      <nav className="mt-5 pl-6">
         <Link to='/dashboard'>Dashboard</Link>
       </nav>
     </div>
