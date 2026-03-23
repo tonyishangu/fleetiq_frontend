2367f3f9c776e7b2b72e0f89d54eaddfd39c1d30
tonyishangu <tonyishangu@gmail.com>
2026-03-23 09:50:24 +0300

    setup routes to the corresponding pages

---
+++ src/pages/Fuel.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Fuel = () => {
+  return (
+    <div>Fuel</div>
+  )
+}
+
+export default Fuel
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Dashboard from '../pages/Dashboard'
+import Orders from '../pages/Orders'
+import Trips from '../pages/Trips'
+import Fuel from '../pages/Fuel'
+import Workshop from '../pages/Workshop'
+import Reports from '../pages/Reports'
+import Adminstration from '../pages/Adminstration'
 
 
 const AppRoutes = () => {
   return (
-    <div>AppRoutes</div>
+    <Routes>
+       <Route path="/dashboard" element={<Dashboard />} />
+       <Route path='/orders' element={<Orders />}/>
+       <Route path='/trips' element={<Trips />} />
+       <Route path='/fuel' element={<Fuel />}/>
+       <Route path='/workshop' element={<Workshop />}/>
+       <Route path='/reports' element={<Reports />}/>
+       <Route path='/adminstration' element={<Adminstration />}/>
+    </Routes>
   )
 }
 
